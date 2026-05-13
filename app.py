2cce31d814759a7006cbc3c9faaf5c068dbee309
Durgaprasad <kagithaladurgaprasad@gmail.com>
2026-05-13 20:14:48 +0530

    Add files via upload
    sentiment analysis app using navie bayes

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pickle
+import re
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+# Load vectorizer
+tfidf = pickle.load(open("tfidf.pkl", "rb"))
+
+# Cleaning function
+def clean_text(text):
+
+    text = text.lower()
+
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
+
+# Title
+st.title("Restaurant Review Sentiment Analysis")
+
+# User Input
+review = st.text_area("Enter Your Review")
+
+# Prediction Button
+if st.button("Predict"):
+
+    # Clean text
+    cleaned_review = clean_text(review)
+
+    # Convert into vector
+    vector_input = tfidf.transform([cleaned_review])
+
+    # Predict
+    prediction = model.predict(vector_input)
+
+    # Output
+    if prediction[0] == 1:
+        st.success("Positive Review 😊")
+
+    else:
+        st.error("Negative Review 😞")
